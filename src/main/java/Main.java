d10759a5b67ddf612a57321590a0e117349dd4e1
yotam1708 <yotam1708@gmail.com>
2025-12-11 00:34:41 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -61,3 +61,85 @@ public static Node<Integer> build(int[] a) {
 		return h.getNext();
 	}
 }
+
+
+
+import java.util.*;
+public class Main{
+
+    public static Node<Integer> selectionSort(Node<Integer> head) {
+        if (head == null || head.getNext() == null) {
+            return head; 
+        }
+
+        Node<Integer> sortedHead = null;
+        Node<Integer> sortedTail = null;
+
+        while (head != null) {
+            Node<Integer> minPrev = null;
+            Node<Integer> minNode = head;
+            Node<Integer> current = head;
+            Node<Integer> prev = null;
+
+            while (current != null) {
+                if (current.getValue() < minNode.getValue()) {
+                    minNode = current;
+                    minPrev = prev;
+                }
+                prev = current;
+                current = current.getNext();
+            }
+
+            if (minPrev == null) {
+                head = head.getNext(); 
+            }
+            else {
+                minPrev.setNext(minNode.getNext()); 
+            }
+
+            minNode.setNext(null);
+
+            if (sortedHead == null) {
+                sortedHead = minNode;
+                sortedTail = minNode;
+            }
+            else {
+                sortedTail.setNext(minNode);
+                sortedTail = minNode; 
+            }
+        }
+
+        return sortedHead;
+    }
+    
+///////////////////////////////////////////////////////////////
+    static Scanner input=new Scanner(System.in);
+        public static void main(String[] args) {
+        int[] data = {4,1,30,66,5}; 
+        
+        Node<Integer> originalHead = build(data); 
+
+        System.out.println(originalHead); 
+        
+        Node<Integer> sortedHead = selectionSort(originalHead);
+
+        if (sortedHead != null) {
+             System.out.println(sortedHead); 
+        } else {
+             System.out.println("הרשימה ריקה");
+        }
+    }
+
+//פעולת העזר build
+public static Node<Integer> build(int[] a) {
+		Node<Integer> h = new Node<Integer>(-1);
+		Node<Integer> p = h;
+		Node<Integer> x;
+		for (int i = 0; i < a.length; i++) {
+			x = new Node<Integer>(a[i]);
+			p.setNext(x);
+			p = p.getNext();
+		}
+		return h.getNext();
+	}
+}
